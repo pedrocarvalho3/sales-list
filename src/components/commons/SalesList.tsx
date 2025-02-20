82173fdfd715dfe1cfc0694b451501e1713c40e2
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-02-20 10:37:07 -0300

    feat(saleslist): adding the delete and edit features

--- src/components/commons/SalesList.tsx
+++ src/components/commons/SalesList.tsx
@@ -11,6 +11,9 @@ import {
   TableHeader,
   TableRow,
 } from '../ui/table';
+import { useRouter } from 'next/navigation';
+import { Dialog, DialogContent, DialogTitle } from '../ui/dialog';
+import { Input } from '../ui/input';
 
 interface Sale {
   id: number;
@@ -26,16 +29,35 @@ const initialSales: Sale[] = [
 
 export default function SalesList() {
   const [sales, setSales] = useState<Sale[]>(initialSales);
+  const [editingSale, setEditingSale] = useState<Sale | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
 
   const handleDelete = (id: number) => {
     setSales(sales.filter(sale => sale.id !== id));
   };
 
+  const handleEdit = (sale: Sale) => {
+    setEditingSale(sale);
+    setIsModalOpen(true);
+  };
+
+  const handleSave = () => {
+    if (editingSale) {
+      setSales(prevSales =>
+        prevSales.map(sale =>
+          sale.id === editingSale.id ? editingSale : sale,
+        ),
+      );
+    }
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
+    <main className="w-full">
       <div className="mb-4">
         <Button
-          onClick={() => console.log('Delete product:')}
+          onClick={() => router.push('/sales/new')}
           className="bg-teal-500 transition-all duration-200 hover:bg-teal-400"
         >
           <Plus className="h-4 w-4" />
@@ -70,7 +92,7 @@ export default function SalesList() {
                     variant="outline"
                     size="icon"
                     className="mr-2 bg-teal-200"
-                    onClick={() => console.log('Delete product:', sale.id)}
+                    onClick={() => handleEdit(sale)}
                   >
                     <Pencil className="h-4 w-4" />
                   </Button>
@@ -88,6 +110,40 @@ export default function SalesList() {
           </TableBody>
         </Table>
       </div>
-    </div>
+      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogContent>
+          <DialogTitle className="text-xl">Editar Venda</DialogTitle>
+          {editingSale && (
+            <div>
+              <Input
+                type="text"
+                value={editingSale.name}
+                onChange={e =>
+                  setEditingSale({ ...editingSale, name: e.target.value })
+                }
+                className="w-full border p-2"
+              />
+              <Input
+                type="number"
+                value={editingSale.amount}
+                onChange={e =>
+                  setEditingSale({
+                    ...editingSale,
+                    amount: Number(e.target.value),
+                  })
+                }
+                className="mt-2 w-full border p-2"
+              />
+              <Button
+                onClick={handleSave}
+                className="mt-4 bg-teal-500 hover:bg-teal-400"
+              >
+                Salvar
+              </Button>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
+    </main>
   );
 }
--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,11 +1,5 @@
 import SalesList from '../commons/SalesList';
 
 export default function HomeTemplate() {
-  return (
-    <div className="container mx-auto">
-      <h1 className="mb-4 py-4 text-4xl font-bold">Lista de Vendas</h1>
-
-      <SalesList />
-    </div>
-  );
+  return <SalesList />;
 }
